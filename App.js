de06200881f80b597f6d3ca2cbf3ddb6edce7fa5
Casey Fayard <fayardcasey@gmail.com>
2020-06-06 13:31:24 -0400

    imported logo, made styling changes

--- App.js
+++ App.js
@@ -1,10 +1,12 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Image, StyleSheet, Text, View } from 'react-native';
+import logo from './assets/logo.png'
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>To share a photo from your phone with a friend, just press the button below!</Text>
+      <Image source={{ uri: "https://i.imgur.com/TkIrScD.png" }} style={styles.logo} />
+      <Text style={styles.instructions}>To share a photo from your phone with a friend, just press the button below!</Text>
     </View>
   );
 }
@@ -15,5 +17,15 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  logo: {
+    width: 305,
+    height: 159,
+    marginBottom: 10,
+  },
+  instructions: {
+    color: '#888',
+    fontSize: 18,
+    marginHorizontal: 15,
   },
 });
