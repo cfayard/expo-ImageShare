34c3ad4d559f82ed4b80cdecd8f18cf3eb0a9ffe
Casey Fayard <fayardcasey@gmail.com>
2020-06-06 13:35:35 -0400

    created a button, with alert

--- App.js
+++ App.js
@@ -1,12 +1,19 @@
 import React from 'react';
-import { Image, StyleSheet, Text, View } from 'react-native';
+import { Image, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 import logo from './assets/logo.png'
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Image source={{ uri: "https://i.imgur.com/TkIrScD.png" }} style={styles.logo} />
-      <Text style={styles.instructions}>To share a photo from your phone with a friend, just press the button below!</Text>
+      <Text style={styles.instructions}>
+        To share a photo from your phone with a friend, just press the button below!
+      </Text>
+      <TouchableOpacity
+        onPress={() => alert('Hello, world!')}
+        style={{backgroundColor: 'blue' }}>
+          <Text style={{ fontSize: 20, color: '#fff'}}>Pick a photo</Text>
+      </TouchableOpacity>
     </View>
   );
 }
